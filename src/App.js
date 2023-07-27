e4479e7c09501c237dc15f8cc6eec1cc451cfca8
kt <cjtakhar@gmail.com>
2023-07-26 21:56:03 -0700

    v 1.0.0

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
       <BrowserRouter>
         < Header />
         <Routes>
-          <Route path="/" element={< Home />} />
+          <Route path="/circuscowgirl" element={< Home />} />
         </Routes>
       </BrowserRouter>
     </div>
