7563b0e2bc81088bb2e1764f6c7de820348333f8
kt <cjtakhar@gmail.com>
2023-09-01 16:28:54 -0700

    update podcast

--- src/components/news.jsx
+++ src/components/news.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
+import '../styles/news.css';
 
 const News = () => {
   return (
     <div className="news">
       <h1 className="title">News</h1>
-      <iframe
-        title="Podcast Episode"
-        src="https://embed.podcasts.apple.com/us/podcast/id1525271300?i=1000550118124"
-        width="100%"
-        height="200"
-        frameborder="0"
-        scrolling="no"
-      ></iframe>
+
+      <div className="podcast-container">
+        <iframe className="podcast-iframe" src="https://embed.podcasts.apple.com/us/podcast/id1525271300?i=1000550118124"></iframe>
+      </div>
+
+      <div className="podcast-container">
+        <iframe className="podcast-iframe" src="https://player.captivate.fm/episode/a70453a8-1f0a-4f94-83bb-1d8c8509cc4d"></iframe>
+      </div>
     </div>
   );
 };
 
 export default News;
+
