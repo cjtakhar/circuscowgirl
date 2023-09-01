c9013f0e8a8df5cc54cdd6c21df28fc92b3508c6
kt <cjtakhar@gmail.com>
2023-09-01 16:31:45 -0700

    update

--- src/components/clinics.jsx
+++ src/components/clinics.jsx
@@ -3,10 +3,10 @@ import Image from '../images/kansas-clinic.jpeg';
 const Clinics = () => {
     return (
         <div className="clinics-container">
+            <h1 className="title">Clinics</h1>
             <div className="clinics-image-container">
                 <img src={Image} alt="Kansas Carradine" className="clinics-image"/>
             </div>
-            <h1 className="title">Clinics</h1>
         </div>
     )
 }
--- src/components/news.jsx
+++ src/components/news.jsx
@@ -3,15 +3,16 @@ import '../styles/news.css';
 
 const News = () => {
   return (
-    <div className="news">
+    <div className="news-container">
       <h1 className="title">News</h1>
-
+      <div className="news"> 
       <div className="podcast-container">
         <iframe className="podcast-iframe" src="https://embed.podcasts.apple.com/us/podcast/id1525271300?i=1000550118124"></iframe>
       </div>
 
       <div className="podcast-container">
         <iframe className="podcast-iframe" src="https://player.captivate.fm/episode/a70453a8-1f0a-4f94-83bb-1d8c8509cc4d"></iframe>
+      </div>
       </div>
     </div>
   );
