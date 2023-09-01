b61295c771c3b982844936b32895659f95ee9ad0
kt <cjtakhar@gmail.com>
2023-09-01 16:17:03 -0700

    podcast

--- src/components/news.jsx
+++ src/components/news.jsx
@@ -1,9 +1,19 @@
+import React from 'react';
+
 const News = () => {
-    return (
-        <div className="news">
-        <h1 className="title">News</h1>
-        </div>
-    );
-}
+  return (
+    <div className="news">
+      <h1 className="title">News</h1>
+      <iframe
+        title="Podcast Episode"
+        src="https://embed.podcasts.apple.com/us/podcast/id1525271300?i=1000550118124"
+        width="100%"
+        height="200"
+        frameborder="0"
+        scrolling="no"
+      ></iframe>
+    </div>
+  );
+};
 
 export default News;
