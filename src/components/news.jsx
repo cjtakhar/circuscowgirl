684cd7cde9236f1b375820dd74f6f107e32de3c6
kt <cjtakhar@gmail.com>
2023-09-01 17:05:24 -0700

    update

--- src/components/news.jsx
+++ src/components/news.jsx
@@ -7,11 +7,19 @@ const News = () => {
       <h1 className="title">News</h1>
       <div className="news"> 
       <div className="podcast-container">
-        <iframe className="podcast-iframe" src="https://embed.podcasts.apple.com/us/podcast/id1525271300?i=1000550118124"></iframe>
+        <iframe 
+          className="podcast-iframe" 
+          src="https://embed.podcasts.apple.com/us/podcast/id1525271300?i=1000550118124"
+          title="The Journey On Podcast">
+        </iframe>
       </div>
 
       <div className="podcast-container">
-        <iframe className="podcast-iframe" src="https://player.captivate.fm/episode/a70453a8-1f0a-4f94-83bb-1d8c8509cc4d"></iframe>
+        <iframe 
+          className="podcast-iframe" 
+          src="https://player.captivate.fm/episode/a70453a8-1f0a-4f94-83bb-1d8c8509cc4d"
+          title="Hands On Gloves Podcast">
+          </iframe>
       </div>
       </div>
       <div className="booking-container">
