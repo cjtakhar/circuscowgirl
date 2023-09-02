c52cd4e9b812ee73f1a7718442706440e4d5f0f2
kt <cjtakhar@gmail.com>
2023-09-01 17:55:53 -0700

    update

--- src/components/heartmath.jsx
+++ src/components/heartmath.jsx
@@ -1,12 +1,16 @@
 import { Link } from 'react-router-dom';
-import Image from '../images/Fyera.jpeg';
+import Image1 from '../images/Fyera.jpeg';
+import Image2 from '../images/heartambassadors.jpeg';
 
 const HeartMath = () => {
     return (
         <div>
             <h1 className="title">HeartMath</h1>
             <div className="hm-image-container">
-                <img src={Image} alt="Kansas Carradine" className="hm-image"/>
+                <img src={Image1} alt="Kansas Carradine" className="hm-image"/>
+            </div>
+            <div className="hm-image-container">
+                <img src={Image2} alt="Kansas Carradine" className="hm-image"/>
             </div>
             <div className="booking-container">
                 < Link to="/book">
