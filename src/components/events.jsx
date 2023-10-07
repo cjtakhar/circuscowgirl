29c99bba1bbb004db4eee6c405772da535fa49be
kt <cjtakhar@gmail.com>
2023-10-06 17:08:11 -0700

    update

--- src/components/events.jsx
+++ src/components/events.jsx
@@ -1,23 +1,57 @@
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import Image1 from '../images/heartmathhorsemanship.jpeg';
-import Image2 from '../images/kansas-clinic-flyer.jpeg';
+import Image1 from '../images/circuscowgirlclinic.png';
+import Image2 from '../images/HeartMathHorsemanship.png';
+import Image3 from '../images/heartmathhorsemanship.jpeg';
+import Image4 from '../images/kansas-clinic-flyer.jpeg';
 import '../styles/events.css';
 
 const Events = () => {
-    return (
-        <div className="events-container">
-            <h1 className="title">Events</h1>
-            <div className="events">
-                <img src={Image1} alt="Kansas Carradine" className="events-image"/>
-                <img src={Image2} alt="Kansas Carradine" className="events-image"/>
-            </div>
-            <div className="booking-container">
-                < Link to="/book">
-                <button className="pay-btn">Book</button>
-                </Link>
-            </div>
-        </div>
-    )
-}
+  const [modalImage, setModalImage] = useState(null);
 
-export default Events
+  // Function to open the modal
+  const openModal = (image) => {
+    setModalImage(image);
+  };
+
+  // Function to close the modal
+  const closeModal = () => {
+    setModalImage(null);
+  };
+
+  return (
+    <div className="events-container">
+      <h1 className="title">Events</h1>
+      <div className="events">
+        {/* Map through your images and display them */}
+        {[Image1, Image2, Image3, Image4].map((image, index) => (
+          <div className="events-image-container" key={index}>
+            <img
+              src={image}
+              alt={`Event ${index + 1}`}
+              className="events-image"
+              onClick={() => openModal(image)}
+            />
+          </div>
+        ))}
+      </div>
+      <div className="booking-container">
+        <Link to="/book">
+          <button className="pay-btn">Book</button>
+        </Link>
+      </div>
+
+      {/* Modal for full-size image */}
+      {modalImage && (
+        <div className="modal" onClick={closeModal}>
+          <span className="close-button" onClick={closeModal}>
+            &times;
+          </span>
+          <img src={modalImage} alt="Full-size Event" className="modal-content" />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Events;
